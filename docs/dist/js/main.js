48b3ac68e8830c66b716ca0ac0afbdd53c4ba4c1
unknown <Octavi Font>
2018-09-16 13:40:32 +0200

    Added static site for nbsharing

---
+++ docs/dist/js/main.js
@@ -0,0 +1,66 @@
+window.addEventListener("dragover",function(e){
+e = e || event;
+e.preventDefault();
+},false);
+
+window.addEventListener("drop",function(e){
+e = e || event;
+e.preventDefault();
+},false);
+
+var dragCounter = 0;
+
+$(document).ready(function() {
+$('#dropzone').on('dragenter', function(e){
+    dragCounter++
+    e.preventDefault()
+    $(this).addClass('active');
+});
+
+$('#dropzone').on('dragleave', function(e){
+    dragCounter--;
+    e.preventDefault()
+    if (dragCounter === 0) {
+    $(this).removeClass('active');
+    }
+});
+
+$('#dropzone').on('dragend', function(e){
+    e.preventDefault()
+});
+
+$('#dropzone').on('drop', function(e){
+    e.preventDefault();
+    dragCounter = 0;
+    $(this).removeClass('active');
+    $("#notebookFileUpload")[0].files = e.originalEvent.dataTransfer.files
+    $("#uploadButton").click()
+});
+
+$("#notebookFileUpload").on("change", function() {
+    var filename = $("#notebookFileUpload")[0].files[0].name;
+    $("#notebookFileUploadLabel").text(filename)
+})
+$("#uploadButton").on("click", function() {
+    if ($("#notebookFileUpload")[0].files.length === 0) {
+    return;
+    }
+    $("#notebookLink").fadeTo("fast", 0)
+    $(".overlay").removeClass("d-none")
+    var formData = new FormData();
+    formData.append("notebook", $("#notebookFileUpload")[0].files[0])
+    fetch("/api/convert", {
+    body: formData,
+    method: "post"
+    }).then(function(response) {
+    $(".overlay").addClass("d-none")
+    return response.text().then(function(text) {
+        $("#notebookFileUpload").val("")
+        $("#notebookFileUploadLabel").text("Select a notebook")
+        $("#notebookLink").val(text)
+        $("#notebookLink").fadeTo("slow", 1)
+    })
+    })
+})
+
+})
